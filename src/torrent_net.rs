b4ed2a91ad8e8fd6fff8857b71b3437d716b5e23
aurelien-cuvelier <aurelien.cuvelier@gmail.com>
2026-02-15 14:02:16 +0300

    TCP handshake with peer

--- src/torrent_net.rs
+++ src/torrent_net.rs
@@ -1,7 +1,9 @@
 use reqwest;
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpStream};
 use urlencoding::encode_binary;
 
-use crate::client::PEER_ID;
+use crate::client;
 use crate::torrent_file::TorrentFile;
 use crate::tracker_data::TrackerData;
 
@@ -11,7 +13,7 @@ pub fn get_announce(torrent_file: TorrentFile) {
         "{}?info_hash={}&peer_id={}&port={}&uploaded={}&downloaded={}&left={}&event={}&compact=1",
         torrent_file.announce,
         encode_binary(torrent_file.info_hash.as_slice()),
-        PEER_ID,
+        client::PEER_ID,
         6881,
         0,
         0,
@@ -40,4 +42,57 @@ pub fn get_announce(torrent_file: TorrentFile) {
     let tracker_data = TrackerData::from(Vec::from(body));
 
     println!("{:?}", tracker_data);
+
+    connect_to_peer(&torrent_file, &tracker_data.peers_str[0]);
+}
+
+fn get_handshake_data(info_hash: &[u8; 20]) -> [u8; 68] {
+    /*
+    * Bytes	Content
+        1	Protocol string length = 19
+        19	BitTorrent protocol (literal ASCII)
+        8	Reserved (all zeros: 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00)
+        20	Info hash (your torrent’s 20-byte info hash)
+        20	Peer ID (your 20-byte peer id)
+    */
+    let mut buf = [0u8; 68];
+
+    buf[0] = 19;
+    buf[1..20].copy_from_slice(b"BitTorrent protocol");
+    //20 => 28 is alrweady 0;
+    buf[28..48].copy_from_slice(info_hash);
+    buf[48..68].copy_from_slice(client::PEER_ID.as_ref());
+    return buf;
+}
+
+pub fn connect_to_peer(torrent_file: &TorrentFile, peer: &str) {
+    //handshakes are 68 bytes long
+    let mut stream = TcpStream::connect(peer).unwrap();
+
+    let handshake_data = get_handshake_data(&torrent_file.info_hash);
+    println!(
+        "\n\nsending handshake to {} data {:?}",
+        peer,
+        String::from_utf8_lossy(&handshake_data)
+    );
+    stream.write_all(&handshake_data).unwrap();
+
+    let mut response = [0u8; 68];
+    stream.read_exact(&mut response).unwrap();
+
+    println!(
+        "\n\nreceived handshake response: {:?}",
+        String::from_utf8_lossy(&response)
+    );
+
+    let peer_id = &response[48..68];
+    let info_hash = &response[28..48];
+
+    println!(
+        "\n\n{peer} - {}\ninfo hash match: {}",
+        String::from_utf8(peer_id.to_vec()).unwrap(),
+        torrent_file.info_hash.eq(info_hash)
+    );
+
+    stream.shutdown(Shutdown::Both).unwrap();
 }
